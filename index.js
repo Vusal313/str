24cdb5f69f23c9173f8e34b7c04fab8a8ea87bc3
Vusal313 <muradovvusal565@gmail.com>
2024-07-30 22:54:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// function colorblue(){
+//     document.getElementById('color').style.color = 'blue'
+// }
+// function colorred(){
+//     document.getElementById('color').style.color = 'red'
+// }
+// function colorblueviolet(){
+//     document.getElementById('color').style.color = 'blueviolet'
+// }
+
+// var x = 15,y =6, cvb = x+y;
+
+// document.write(cvb);
+
+// for(let i = 0;i<51;i+=5){
+// console.log(i);
+// }
+// var x =5;
+// document.write(x)
+
+// x += 5;
+// document.write(x)
+
+// var txt = 'Hello World';
+// console.log(txt);
+// txt += ' Necesen '
+// console.log(txt);
+
+// var x = 10;
+// if (x < 15 || x > 11) {
+//   console.log("tamam");
+// } else {
+//   console.log("yok");
+// }
+
+// let ad = "vusal";
+// let soyad = "muradov";
+// let is = "devoloper";
+
+// let say = 25;
+
+// let islem = true;
+// let other = false;
+
+// const adlar =['vusal',[1,2,32,3],'suleyman','Aybeniz',25,44,56]
+
+// const personel = {
+// ad:'vusal',soyad:'muradov',is:'devoloper'
+// }
+
+
